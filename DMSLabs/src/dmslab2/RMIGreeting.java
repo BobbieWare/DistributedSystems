13159f6d4d038a845fecc16e60049e2d47884a70
BobbieWare <robbie.ware@gmail.com>
2019-03-24 19:33:21 +1300

    Lab work

---
+++ DMSLabs/src/dmslab2/RMIGreeting.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmslab2;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RMIGreeting extends Remote{
+    public String getGreeting() throws RemoteException;
+    public void setGreeting(String message) throws RemoteException;
+}
