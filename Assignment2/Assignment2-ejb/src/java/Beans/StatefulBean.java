62c3d989ffceaf778dd1e57e5e2e0fb9606a38c7
BobbieWare <robbie.ware@gmail.com>
2019-04-17 14:35:01 +1200

    Start of a2

---
+++ Assignment2/Assignment2-ejb/src/java/Beans/StatefulBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import javax.ejb.Stateful;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Bob
+ */
+@Stateful
+@LocalBean
+public class StatefulBean
+{
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
